96feeb42826b9969ef631f538c87802c2147817a
sveram <svera@intelnexo.com>
2024-05-26 13:14:45 -0500

    updt: actualizar modelos espaciales generico

--- gisweb/admin.py
+++ gisweb/admin.py
@@ -3,6 +3,13 @@ from .models import *
 
 from leaflet.admin import LeafletGeoAdmin
 
+class GISDetailInline(admin.TabularInline):
+    model = GISDetail
+    extra = 10  # Número de detalles adicionales que se pueden agregar
+
+class GISModelAdmin(LeafletGeoAdmin):
+    inlines = [GISDetailInline]
+
 admin.site.register(TypeSector, LeafletGeoAdmin)
 admin.site.register(Sector, LeafletGeoAdmin)
 admin.site.register(Category, LeafletGeoAdmin)
@@ -10,6 +17,6 @@ admin.site.register(SubCategory, LeafletGeoAdmin)
 admin.site.register(Indicator, LeafletGeoAdmin)
 admin.site.register(TypeSource, LeafletGeoAdmin)
 admin.site.register(IndicatorData, LeafletGeoAdmin)
-admin.site.register(GISModel, LeafletGeoAdmin)
+admin.site.register(GISModel, GISModelAdmin)
 admin.site.register(GISDetail, LeafletGeoAdmin)
 
